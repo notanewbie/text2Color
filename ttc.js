c7f42e7250de15e382109d1fb55c67ce7e905280
notanewbie <computerchipcity@aol.com>
2019-05-09 12:21:50 -0400

    Create ttc.js

---
+++ ttc.js
@@ -0,0 +1,8 @@
+function text2Color(text) {
+n = text.charCodeAt(0)+text.charCodeAt(text.length - 1);
+n = n * (360/172)
+a = n.toString()[0];
+b = n.toString()[1];
+c = n.toString()[2];
+return "hsl(" + n + ", 100%, 50%)";
+}
